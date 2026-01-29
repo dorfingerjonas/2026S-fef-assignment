96044eb3a0c0055c88eb6dc747ae7dda35285cf9
Jonas Dorfinger <contact@dorfingerjonas.at>
2026-01-29 14:28:07 +0100

    add prop imageFileName to item model

--- data/items.js
+++ data/items.js
@@ -3,6 +3,7 @@ const items = [
         id: -1,
         name: 'Sample Item',
         description: 'This is a sample item for demonstration purposes.',
+        imageFileName:'',
         price: 9.99,
         category: 'Sample Category'
     }
